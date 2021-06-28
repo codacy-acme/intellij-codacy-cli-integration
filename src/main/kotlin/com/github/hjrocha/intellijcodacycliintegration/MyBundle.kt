355c6ec0d8fee326974dc100e9f8828a208e8a45
GitHub Action <action@github.com>
2021-06-28 10:57:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/hjrocha/intellijcodacycliintegration/MyBundle.kt
+++ src/main/kotlin/com/github/hjrocha/intellijcodacycliintegration/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hjrocha.intellijcodacycliintegration
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/hjrocha/intellijcodacycliintegration/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.hjrocha.intellijcodacycliintegration.services
+
+import com.github.hjrocha.intellijcodacycliintegration.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
